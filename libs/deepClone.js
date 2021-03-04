f9fc4d9cbccb2cc0d4363d7008f204bcd85bbdbb
ll799268 <1010023095@qq.com>
2021-03-04 16:25:49 +0800

    add deepClone

---
+++ libs/deepClone.js
@@ -0,0 +1,23 @@
+/**
+ * 深度克隆(不包含日期、正则)
+ * @param {*} origin 
+ * @param {*} target 
+ */
+function deepClone(origin, target) {
+  var tar = target || {},
+    toStr = Object.prototype.toString,
+    arrType = '[object Array]'
+
+  for (var k in origin) {
+    if (origin.hasOwnProperty(k)) {
+      if (typeof origin[k] === 'object' && origin[k] !== null) {
+        tar[k] = toStr.call(origin[k]) === arrType ? [] : {}
+        deepClone(origin[k], tar[k])
+      } else {
+        tar[k] = origin[k]
+      }
+    }
+  }
+
+  return tar
+}
