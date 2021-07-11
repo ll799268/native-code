d05ce44363c20fc4ab930f2dc886e2b9c8ccf026
ll799268 <1010023095@qq.com>
2021-07-11 20:44:41 +0800

    add messag

--- libs/proxySearchArr.js
+++ libs/proxySearchArr.js
@@ -1,14 +1,20 @@
 let originArr = [1, 12, 4, 6, 8, 10]
 
+/**
+ * 通过es6代理查询数组的下标 负数代表倒数第几个
+ * @param {*} arr 
+ * @returns 
+ */
+
 function proxySearchArr(arr) {
   let proxy = new Proxy(arr, {
     // 源对象、下标/字段，代理过的对象
     get(oTarget, sKey, receiver) {
-      console.log(receiver);
       if (sKey >= 0) {
         return oTarget[sKey]
       } else {
         return oTarget[Math.abs(sKey)]
+        // return oTarget[arr.length + parseInt(sKey)]
       }
     }
   })
@@ -18,4 +24,4 @@ function proxySearchArr(arr) {
 
 const formatArr = proxySearchArr(originArr)
 
-console.log(formatArr[-1]);
+console.log(formatArr[-3]);
