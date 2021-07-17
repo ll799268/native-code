5eab29b6335698f777b76cf2338ab9835fef9eef
ll799268 <1010023095@qq.com>
2021-07-17 17:13:17 +0800

    config scss

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ export default {
   server: {
     open: '/index.html',
     host: '0.0.0.0',
+  },
+  cssPreprocessOptions: {
+    scss: {
+      additionalData: '@import "./src/css/waterfallFlow.scss";' // 添加公共样式
+    }
   }
 }
