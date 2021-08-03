d656a6ec03e388e4652a9698d0d4b5181a2dec35
ll799268 <1010023095@qq.com>
2021-08-03 09:35:02 +0800

    clone Promise

--- libs/myPromise.js
+++ libs/myPromise.js
@@ -1,82 +1,165 @@
-// 状态
-const STATUS = {
-  FULFILLED: 'resolved',
-  REJECTED: 'rejected',
-  PENDING: 'pending'
-}
-
-
 /**
- * 发布者订阅模式（简单可以调用）
+ * 模拟实现Promise
+ * Promise利用三大手段解决回调地狱：
+ * 1. 回调函数延迟绑定
+ * 2. 返回值穿透
+ * 3. 错误冒泡
  */
-class myPromise {
-  constructor(executor) {
-    this.status = STATUS.PENDING
 
-    this.value = undefined // 成功的数据
-    this.reason = undefined  // 失败的原因
+// 定义三种状态
+const PENDING = 'PENDING';      // 进行中
+const FULFILLED = 'FULFILLED';  // 已成功
+const REJECTED = 'REJECTED';    // 已失败
 
-    this.onResolvedCallbacks = [] // 成功回调队列
-    this.onRejectedCallbacks = [] // 失败回调队列
+class Promise {
+  constructor(exector) {
+    // 初始化状态
+    this.status = PENDING;
+    // 将成功、失败结果放在this上，便于then、catch访问
+    this.value = undefined;
+    this.reason = undefined;
+    // 成功态回调函数队列
+    this.onFulfilledCallbacks = [];
+    // 失败态回调函数队列
+    this.onRejectedCallbacks = [];
 
-    let resolve = val => {
-      // 状态不可逆，只有是PENDING时，才可以改变状态
-      if (this.status === STATUS.PENDING) {
-        this.status = STATUS.RESOLVED
-        this.value = val
-
-        this.onResolvedCallbacks.forEach(fn => fn())
+    const resolve = value => {
+      // 只有进行中状态才能更改状态
+      if (this.status === PENDING) {
+        this.status = FULFILLED;
+        this.value = value;
+        // 成功态函数依次执行
+        this.onFulfilledCallbacks.forEach(fn => fn(this.value));
       }
     }
 
-    let reject = reason => {
-      // 状态不可逆，只有是PENDING时，才可以改变状态
-      if (this.status === STATUS.PENDING) {
-        this.status = STATUS.REJECTED
-        this.reason = reason
-        
-        this.onRejectedCallbacks.forEach(fn => fn())
+    const reject = reason => {
+      // 只有进行中状态才能更改状态
+      if (this.status === PENDING) {
+        this.status = REJECTED;
+        this.reason = reason;
+        // 失败态函数依次执行
+        this.onRejectedCallbacks.forEach(fn => fn(this.reason))
       }
     }
-
-    // 错误处理
     try {
-      executor(resolve, reject)
-    } catch (err) {
-      reject(err)
+      // 立即执行executor
+      // 把内部的resolve和reject传入executor，用户可调用resolve和reject
+      exector(resolve, reject);
+    } catch (e) {
+      // executor执行出错，将错误内容reject抛出去
+      reject(e);
     }
-
   }
-
-  // 实例的公共方法
+  
   then(onFulfilled, onRejected) {
-    if (this.status === STATUS.FULFILLED) {
-      onFulfilled(this.value)
-    }
-    if (this.status === STATUS.REJECTED) {
-      onRejected(this.reason)
-    }
-    if (this.status === STATUS.PENDING) {
-      this.onResolvedCallbacks.push(() => {
-        onFulfilled(this.value)
-      })
-      this.onRejectedCallbacks.push(() => {
-        onRejected(this.reason)
-      })
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+    onRejected = typeof onRejected === 'function' ? onRejected :
+      reason => { throw new Error(reason instanceof Error ? reason.message : reason) }
+    // 保存this
+    const self = this;
+    return new Promise((resolve, reject) => {
+      if (self.status === PENDING) {
+        self.onFulfilledCallbacks.push(() => {
+          // try捕获错误
+          try {
+            // 模拟微任务
+            setTimeout(() => {
+              const result = onFulfilled(self.value);
+              // 分两种情况：
+              // 1. 回调函数返回值是Promise，执行then操作
+              // 2. 如果不是Promise，调用新Promise的resolve函数
+              result instanceof Promise ? result.then(resolve, reject) : resolve(result);
+            })
+          } catch (e) {
+            reject(e);
+          }
+        });
+        self.onRejectedCallbacks.push(() => {
+          // 以下同理
+          try {
+            setTimeout(() => {
+              const result = onRejected(self.reason);
+              // 不同点：此时是reject
+              result instanceof Promise ? result.then(resolve, reject) : resolve(result);
+            })
+          } catch (e) {
+            reject(e);
+          }
+        })
+      } else if (self.status === FULFILLED) {
+        try {
+          setTimeout(() => {
+            const result = onFulfilled(self.value);
+            result instanceof Promise ? result.then(resolve, reject) : resolve(result);
+          });
+        } catch (e) {
+          reject(e);
+        }
+      } else if (self.status === REJECTED) {
+        try {
+          setTimeout(() => {
+            const result = onRejected(self.reason);
+            result instanceof Promise ? result.then(resolve, reject) : resolve(result);
+          })
+        } catch (e) {
+          reject(e);
+        }
+      }
+    });
+  }
+
+  catch(onRejected) {
+    return this.then(null, onRejected);
+  }
+
+  static resolve(value) {
+    if (value instanceof Promise) {
+      // 如果是Promise实例，直接返回
+      return value;
+    } else {
+      // 如果不是Promise实例，返回一个新的Promise对象，状态为FULFILLED
+      return new Promise((resolve, reject) => resolve(value));
     }
   }
 
+  static reject(reason) {
+    return new Promise((resolve, reject) => {
+      reject(reason);
+    })
+  }
+
+  static all(promiseArr) {
+    const len = promiseArr.length;
+    const values = new Array(len);
+    // 记录已经成功执行的promise个数
+    let count = 0;
+    return new Promise((resolve, reject) => {
+      for (let i = 0; i < len; i++) {
+        // Promise.resolve()处理，确保每一个都是promise实例
+        Promise.resolve(promiseArr[i]).then(
+          val => {
+            values[i] = val;
+            count++;
+            // 如果全部执行完，返回promise的状态就可以改变了
+            if (count === len) resolve(values);
+          },
+          err => reject(err),
+        );
+      }
+    })
+  }
+
+  static race(promiseArr) {
+    return new Promise((resolve, reject) => {
+      promiseArr.forEach(p => {
+        Promise.resolve(p).then(
+          val => resolve(val),
+          err => reject(err),
+        )
+      })
+    })
+  }
 }
 
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('成功');
-  },1000);
-}).then(
-  (data) => {
-    console.log('success', data)
-  },
-  (err) => {
-    console.log('faild', err)
-  }
-)
+
