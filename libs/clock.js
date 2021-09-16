546411aea90eb3cbe84fe86c076684349aa05221
ll799268 <1010023095@qq.com>
2021-09-16 17:34:26 +0800

    add animation clock

---
+++ libs/clock.js
@@ -0,0 +1,16 @@
+;(function() {
+
+  const initDmo = () => {
+    document.body.innerHTML = `
+      <div class="clock">
+        <div class="top-radius">
+        </div>
+        <div class="clock-body">
+          <div class="line"></div>
+          <div class="small-radius"></div>
+        </div>
+      </div>`
+  }
+
+  initDmo()
+})()
