d60be8880ee85521d774cbcb1a408001e833192c
ll799268 <1010023095@qq.com>
2022-04-23 18:55:21 +0800

    update curry

--- libs/curry.js
+++ libs/curry.js
@@ -1,17 +1,12 @@
-/**
- * 函数柯里化
- * @param {*} fn 
- * @param {*} args 
- * @returns 
- */
-function curry(fn, args = []) {
-  return function () {
-    // var newArgs = args.concat(Array.prototype.slice.call(arguments)) // 参数是伪数组，将其转换为数组
-    const newArgs = [...args, ...arguments] // 参数是伪数组，将其转换为数组
-    if (newArgs.length < fn.length) {
-      curry.call(this, fn, newArgs)
-    } else {
-      fn.apply(this, newArgs)
-    }
+const curry = (fn, arg = []) => {
+  return () => {
+    const newArg = [...arg, arguments]
+    newArg.length < fn.length ? fn.apply(this, newArg) :  curry.call(this, fn, newArg)
   }
 }
+
+const add = arr => arr.reduce((prev, cur) => prev + cur, 0)
+
+const sum = add([1, 2, 3])
+
+console.log(sum);
